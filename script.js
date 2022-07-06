b7d7589e8e4d64199f51bd5dae8fcafe343c9cfa
lightningund <lightningundies0826@gmail.com>
2022-07-06 12:52:44 -0500

    created the function that searches the word list

--- script.js
+++ script.js
@@ -13,5 +13,20 @@ const ctxt = canv.getContext("2d");
  * @returns {string[]}
  */
 const get_words_from_letters = letters => {
+	const valid_words = [];
 
+	for(const word of words) {
+		if(word.length > letters.length) continue;
+
+		const word_arr = word.split("");
+		const valid = word_arr.every(letter => letters.includes(letter)
+			&& word_arr.filter(el => el === letter).length <= letters.filter(el => el === letter).length
+		);
+
+		if(valid) valid_words.push(word);
+	}
+
+	return valid_words;
 }
+
+console.log(get_words_from_letters(["t", "h", "i", "n", "k"]));
